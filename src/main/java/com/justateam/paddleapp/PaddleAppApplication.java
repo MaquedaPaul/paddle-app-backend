6a391cc6b5538717982072461522c006f3fce6c3
MaquedaPaul <maquedapablodavid@gmail.com>
2024-02-21 15:50:59 -0300

    Initial project I've added the necessary files to start working in the project

---
+++ src/main/java/com/justateam/paddleapp/PaddleAppApplication.java
@@ -0,0 +1,13 @@
+package com.justateam.paddleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaddleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justateam/paddleapp/PaddleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justateam.paddleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
